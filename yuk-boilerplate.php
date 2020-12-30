9251cbf3b84e4048fe0d209623cd453da7382076
naingood <nain.daftar@gmail.com>
2020-12-30 16:31:36 +0700

    update header

---
+++ yuk-boilerplate.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+Plugin Name: Nama Plugin
+Plugin URI: http://www.yukdiorder.com
+Description: Keterangan plugin 
+Version: 0.1
+Author: Isnainy Nasrullah
+Author URI: http://www.yukdiorder.com
+Text-domain: yuk-boilerplate
+
+
+*/ 
+?>
